9e42c1db19dfb282540823fd3d5954ca863290de
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-12-01 18:30:55 +0100

    1.a

--- 1.ts
+++ 1.ts
@@ -1,1 +1,13 @@
 import { solve } from "./solve";
+
+solve((input) => {
+  const numbers = input.split(/\n/).map((num) => parseInt(num, 10));
+  const { increases } = numbers.reduce(
+    ({ increases, prev }, curr) => ({
+      increases: increases + Number(curr > prev && prev !== undefined),
+      prev: curr,
+    }),
+    { increases: 0, prev: undefined }
+  );
+  return increases;
+});
